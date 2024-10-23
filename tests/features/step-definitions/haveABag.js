b4d2f90ab0d197e5347f110afe7c1d60da70ad7e
SakuyaYae <darkreimu@outlook.com>
2024-10-23 13:14:16 +0200

    Update haveABag.js

--- tests/features/step-definitions/haveABag.js
+++ tests/features/step-definitions/haveABag.js
@@ -3,36 +3,37 @@ import { By, until, Key } from 'selenium-webdriver';
 import { expect } from "chai";
 
 Given('i have clicked on the help button', async function () {
-  const helpBtn = await this.driver.findElements(By.xpath("//*[contains(text(),'Help')]"));
+  const helpBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Help')]"));
   await helpBtn[0].click();
 });
 
 Given('i have clicked on go south', async function () {
-  const southBtn = await this.driver.findElements(By.xpath("//*[contains(text(),'Go south')]"));
+  const southBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Go south')]"));
   await southBtn[0].click();
 });
 
 Given('i have clicked on go north', async function () {
-  const northBtn = await this.driver.findElements(By.xpath("//*[contains(text(),'Go north')]"));
+  const northBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Go north')]"));
   await northBtn[0].click();
 });
 
-
 When('i click on go south', async function () {
-  const southBtn = await this.driver.findElements(By.xpath("//*[contains(text(),'Go south')]"));
+  const southBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Go south')]"));
   await southBtn[0].click();
 });
 
 When('i click on go west', async function () {
-  const westBtn = await this.driver.findElements(By.xpath("//*[contains(text(),'Go west')]"));
+  const westBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Go west')]"));
   await westBtn[0].click();
 });
+
 When('i click on go east', async function () {
-  const eastBtn = await this.driver.findElements(By.xpath("//*[contains(text(),'Go east')]"));
+  const eastBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Go east')]"));
   await eastBtn[0].click();
 });
+
 When('i click on go north', async function () {
-  const northBtn = await this.driver.findElements(By.xpath("//*[contains(text(),'Go north')]"));
+  const northBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Go north')]"));
   await northBtn[0].click();
 });
 
