0fc9c3e20abd76267cea7193f337b898fc19239f
AnnelieLinden <lia.akesson@gmail.com>
2024-10-24 13:33:41 +0200

    Update spendingMoney.js

--- tests/features/step-definitions/spendingMoney.js
+++ tests/features/step-definitions/spendingMoney.js
@@ -4,8 +4,8 @@ import { By, until, Key } from 'selenium-webdriver';
 import { expect } from 'chai';
 
 When('I press Buy an espresso', async function(){
-  const northBtn = await this.driver.findElements(By.xpath("//li[contains(text(),'Buy an espresso')]"));
-  await northBtn[0].click();
+  const buyEspresso = await this.driver.findElements(By.xpath("//li[contains(text(),'Buy an espresso')]"));
+  await buyEspresso[0].click();
 });
 
 Then('My money should be reduced by {float} coins', async function(a){
