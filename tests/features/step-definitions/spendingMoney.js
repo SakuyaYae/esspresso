2b6e167f856ad8a145feb3f88aaebf6945be8dd9
SakuyaYae <darkreimu@outlook.com>
2024-10-24 14:39:39 +0200

    fix

--- tests/features/step-definitions/spendingMoney.js
+++ tests/features/step-definitions/spendingMoney.js
@@ -3,10 +3,14 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import { By, until, Key } from 'selenium-webdriver';
 import { expect } from 'chai';
 
-When('I press Buy an espresso', async function(){
-  // TODO: implement step
+When('I press Buy an espresso', async function () {
+  const buyEspresso = await this.driver.findElements(By.xpath("//li[contains(text(),'Buy an espresso')]"));
+  await buyEspresso[0].click();
 });
 
-Then('My money should be reduced by {float} coins', async function(a){
-  // TODO: implement step
+Then('My money should be reduced by {float} coins', async function (a) {
+  const parent = await this.driver.findElement(By.className("money"));
+  const currentMoney = await parent.findElement(By.className("val"));
+
+  expect(parseInt(await currentMoney.getText())).to.be.lessThan(10);
 });
