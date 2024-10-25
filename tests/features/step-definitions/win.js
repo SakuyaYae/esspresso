b7abcc7a435a926ce7b121c7bb211f8b6f50922b
SakuyaYae <darkreimu@outlook.com>
2024-10-25 08:55:47 +0200

    Update win.js

--- tests/features/step-definitions/win.js
+++ tests/features/step-definitions/win.js
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 Then("I have five espressos and want to see the winning text", async function () {
   const win = await this.driver.findElements(By.className("description"));
   for (var e of win) {
-    expect(await e.getText()).to.contain("Yes! You feel alive and pumping.Full of caffeine! You feel like... like... Luke Skywalker!");
+    expect(await e.getText()).to.contain("Yes! You feel alive and pumping. Full of caffeine! You feel like... like... Luke Skywalker!");
   }
 });
 
